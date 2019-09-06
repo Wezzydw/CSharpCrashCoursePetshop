31a5195b28f40a56181fe5536d1c84057dd822da
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2019-09-06 11:10:46 +0200

    Done w/ CRUD

--- CA_Petshop/UI.RestAPI/Controllers/OwnersController.cs
+++ CA_Petshop/UI.RestAPI/Controllers/OwnersController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CA_Petshop.Core.ApplicationServices;
+using CA_Petshop.Core.Entity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,36 @@ namespace UI.RestAPI.Controllers
     [ApiController]
     public class OwnerController : ControllerBase
     {
+        private readonly IOwnerService _ownerService;
+
+        public OwnerController(IOwnerService ownerService)
+        {
+            _ownerService = ownerService;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Owner owner)
+        {
+            _ownerService.CreateOwner(owner);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Owner>> Get()
+        {
+            return _ownerService.GetOwners();
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Owner owner)
+        {
+            _ownerService.ChangeOwner(owner);
+        }
+
+        // DELETE api/pets/anyID
+        [HttpDelete]
+        public void Delete([FromQuery] int id)
+        {
+            _ownerService.DeleteOwner(id);
+        }
     }
 }
